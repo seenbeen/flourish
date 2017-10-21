6376f1c077464449578f197a651f7807b7ed7c18
Shiyang Han <shiratori.seenbeen@gmail.com>
2017-10-21 20:07:00 +0000

    sweet, graphql working as intended

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('HI JASON!');
-});
+const { graphql, buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+
+const flourishSchema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var rootResolver = {
+  hello: () => {
+    return 'Sweet it werkt!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: flourishSchema,
+  rootValue: rootResolver,
+  graphiql: true
+}));
+
 app.listen(3000, () => console.log('Server running on port 3000'));
